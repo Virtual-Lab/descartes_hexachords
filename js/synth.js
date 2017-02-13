e404b2e7b7334e3e4fea8a3abbb63a3fb3597d58
Vogtenhuber Raimund <voggy@gmx.at>
2017-02-13 11:34:06 +0100

    Update 2017-02-13T10:34:05.065Z

--- js/synth.js
+++ js/synth.js
@@ -133,7 +133,6 @@ Synthesizer.view.draw = function (name, element) {
 
   // append the section to the synthesizer
   this.synthesizer.appendChild(field);
-  this.synthesizer.appendChild(zeile[0  ]);
 
 }
 
